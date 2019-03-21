1d9c9f7786abc10080b5bb07e18d121431f873f6
Anderson Madeira <anderson.madeira@fortaleza.ce.gov.br>
2019-03-21 15:34:19 -0300

    React routing and more vscode config for this project

---
+++ src/components/SectionOne.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SectionOne() {
+  return (
+    <div>
+      <p>SectionOne</p>
+    </div>
+  );
+}
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Home from './Home';
+import SectionOne from './SectionOne';
+import SectionTwo from './SectionTwo';
+
+export { Home, SectionOne, SectionTwo };
