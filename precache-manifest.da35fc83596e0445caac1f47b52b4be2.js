b5ce1073d76861bb3a2b21b3e23e9ae4abe6ee28
Anderson Madeira <anderson.madeira@fortaleza.ce.gov.br>
2019-03-26 16:47:08 -0300

    Updates

---
+++ precache-manifest.da35fc83596e0445caac1f47b52b4be2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2758ee7e916e8a5fc031",
+    "url": "/seapass/static/js/runtime~main.5f3eee5c.js"
+  },
+  {
+    "revision": "0caf2360893e12a3457d",
+    "url": "/seapass/static/js/main.88f5c242.chunk.js"
+  },
+  {
+    "revision": "3f84ae7328a148c5608d",
+    "url": "/seapass/static/js/2.1a9baf9f.chunk.js"
+  },
+  {
+    "revision": "0caf2360893e12a3457d",
+    "url": "/seapass/static/css/main.a2255a87.chunk.css"
+  },
+  {
+    "revision": "3f84ae7328a148c5608d",
+    "url": "/seapass/static/css/2.479de048.chunk.css"
+  },
+  {
+    "revision": "6c52a5f2788b09d19ed52e7b17afa798",
+    "url": "/seapass/index.html"
+  }
+];
