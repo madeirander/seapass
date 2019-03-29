4f2b8ac61c7140d28af4d03b8270f83b6c81d186
Anderson Madeira <anderson.madeira@fortaleza.ce.gov.br>
2019-03-29 10:09:41 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/seapass/precache-manifest.303d4752931abd277aadc0b7313779c2.js"
+  "/seapass/precache-manifest.ec2ccf7f963fbdb43bbfa5dfa906b5ec.js"
 );
 
 workbox.clientsClaim();
