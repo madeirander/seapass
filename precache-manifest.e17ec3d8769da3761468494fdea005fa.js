638b3e99158bfd163f14e2ead6c3caa94459d18d
Anderson Madeira <andersonmadeiracs@gmail.com>
2019-06-06 22:33:27 -0300

    Updates

---
+++ precache-manifest.e17ec3d8769da3761468494fdea005fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e26f23e7a4eb5108fd8c9026c4ab6a",
+    "url": "/seapass/index.html"
+  },
+  {
+    "revision": "ca14aed5d2f1e43cf919",
+    "url": "/seapass/static/css/2.12793046.chunk.css"
+  },
+  {
+    "revision": "a270fa0db57907fa961e",
+    "url": "/seapass/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "ca14aed5d2f1e43cf919",
+    "url": "/seapass/static/js/2.3be901bf.chunk.js"
+  },
+  {
+    "revision": "a270fa0db57907fa961e",
+    "url": "/seapass/static/js/main.0cffbfca.chunk.js"
+  },
+  {
+    "revision": "5fe5db28d4641b5cd688",
+    "url": "/seapass/static/js/runtime~main.d05c4c42.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/seapass/precache-manifest.9a3c51f31f18dfb7cda252495684bdd4.js"
+  "/seapass/precache-manifest.e17ec3d8769da3761468494fdea005fa.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/seapass/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/seapass/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
