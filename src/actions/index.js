d9f8012db5ba07cdacbf2ec7eb4e31e959224e96
Anderson Madeira <anderson.madeira@fortaleza.ce.gov.br>
2019-10-11 11:34:46 -0300

    Project cleaned a little to prepare for new layout

--- src/actions/index.js
+++
@@ -1,6 +0,0 @@
-import { UPDATE_ACTIVE_ENTRY } from './actionTypes'
-
-export const setActiveEntry = value => ({
-  type: UPDATE_ACTIVE_ENTRY,
-  entryId: value,
-})
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import { Provider } from 'react-redux'
-import App from './App'
-import Store from './store'
+import App from './components/App'
 
-ReactDOM.render(
-  <Provider store={Store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.querySelector('#root'))
