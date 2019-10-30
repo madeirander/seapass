c77b5193a4b259b5e2382f4a9465cdeacff704dc
Anderson Madeira <anderson.madeira@fortaleza.ce.gov.br>
2019-10-30 11:50:18 -0300

    Basic entries request

--- src/components/Entries.js
+++ src/components/Entries.js
@@ -1,18 +1,62 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
 import { faFileAlt } from '@fortawesome/free-solid-svg-icons'
 import ContentWrapper from './ContentWrapper'
 import Header from './Header'
 import Content from './Content'
+import { fetchEntries } from '../actions/entries-actions'
 
-const Entries = () => {
+const Entries = props => {
+  const { entries, loading, error, requestEntries } = props
+
+  useEffect(() => {
+    requestEntries()
+  }, [requestEntries])
+
+  let msg = null
+
+  if (loading) {
+    msg = <p>Loading...</p>
+  }
+
+  if (error) {
+    msg = <p style={{ color: 'red' }}>Error loading entries</p>
+  }
+
+  const entriesList = entries
+    ? entries.map(e => (
+        <div key={e._id}>
+          <h3 style={{ marginTop: 0 }}>{e.name}</h3>
+          <p>{e.description}</p>
+          <ul>
+            <li>Hostname: {e.credentials.hostname}</li>
+            <li>Username: {e.credentials.username}</li>
+            <li>Password: {e.credentials.password}</li>
+          </ul>
+        </div>
+      ))
+    : []
+
   return (
     <ContentWrapper>
       <Header title="Entries" icon={faFileAlt} />
       <Content>
-        <p>Some entries lie here</p>
+        {entriesList}
+        {msg}
       </Content>
     </ContentWrapper>
   )
 }
 
-export default Entries
+const mapStateToProps = state => {
+  return {
+    entries: state.entries.items,
+    loading: state.entries.loading,
+    error: state.entries.error,
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { requestEntries: fetchEntries }
+)(Entries)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import authReducer from './auth-reducer'
 import currentUserReducer from './current-user-reducer'
+import entriesReducer from './entries-reducer'
 
 const reducers = combineReducers({
   auth: authReducer,
   currentUser: currentUserReducer,
+  entries: entriesReducer,
 })
 
 export default reducers
